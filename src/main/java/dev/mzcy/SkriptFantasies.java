67e73b97dfa895ab55e84356e34b0a99f8734554
KeineSecrets <miethe.max@web.de>
2024-06-08 09:01:14 +0200

    Added Paper API and added main class SkriptFantasies

---
+++ src/main/java/dev/mzcy/SkriptFantasies.java
@@ -0,0 +1,19 @@
+package dev.mzcy;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SkriptFantasies extends JavaPlugin {
+
+
+
+
+    @Override
+    public void onDisable() {
+
+    }
+
+    @Override
+    public void onEnable() {
+
+    }
+}
